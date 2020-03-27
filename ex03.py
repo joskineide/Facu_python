585d8b8c94a0b528241b242ff50b6d38466e6c7b
default <default>
2020-03-27 19:11:27 -0300

    adding ex 3-20

--- ex03.py
+++ ex03.py
@@ -1,18 +1,19 @@
 import math  
 
 
-# 20. O Sr. Manoel Joaquim possui uma grande loja de artigos de R$ 1,99, com cerca
-# de 10 caixas. Para agilizar o cálculo de quanto cada cliente deve pagar ele
-# desenvolveu um tabela que contém o número de itens que o cliente comprou e ao
-# lado o valor da conta. Desta forma a atendente do caixa precisa apenas contar
-# quantos itens o cliente está levando e olhar na tabela de preços. Você foi contratado
-# para desenvolver o programa que monta esta tabela de preços, que conterá os
-# preços de 1 até 50 produtos, conforme o exemplo abaixo:
-# Lojas Quase Dois - Tabela de preços
-# 1 - R$ 1.99
-# 2 - R$ 3.98
+# 21. O Sr. Manoel Joaquim acaba de adquirir uma panificadora e pretende implantar
+# a metodologia da tabelinha, que já é um sucesso na sua loja de 1,99. Você foi
+# contratado para desenvolver o programa que monta a tabela de preços de pães, de
+# 1 até 50 pães, a partir do preço do pão informado pelo usuário, conforme o exemplo
+# abaixo:
+# Preço do pão: R$ 0.18
+# Panificadora Pão de Ontem - Tabela de preços
+
+# 1 - R$ 0.18
+# 2 - R$ 0.36
 # ...
-# 50 - R$ 99.50
+# 50 - R$ 9.00
+
 
 
 raise "hell"
@@ -477,19 +478,29 @@ while count > 0:
 print(total)
 
 
-
-# 21. O Sr. Manoel Joaquim acaba de adquirir uma panificadora e pretende implantar
-# a metodologia da tabelinha, que já é um sucesso na sua loja de 1,99. Você foi
-# contratado para desenvolver o programa que monta a tabela de preços de pães, de
-# 1 até 50 pães, a partir do preço do pão informado pelo usuário, conforme o exemplo
-# abaixo:
-# Preço do pão: R$ 0.18
-# Panificadora Pão de Ontem - Tabela de preços
-
-# 1 - R$ 0.18
-# 2 - R$ 0.36
+# 20. O Sr. Manoel Joaquim possui uma grande loja de artigos de R$ 1,99, com cerca
+# de 10 caixas. Para agilizar o cálculo de quanto cada cliente deve pagar ele
+# desenvolveu um tabela que contém o número de itens que o cliente comprou e ao
+# lado o valor da conta. Desta forma a atendente do caixa precisa apenas contar
+# quantos itens o cliente está levando e olhar na tabela de preços. Você foi contratado
+# para desenvolver o programa que monta esta tabela de preços, que conterá os
+# preços de 1 até 50 produtos, conforme o exemplo abaixo:
+# Lojas Quase Dois - Tabela de preços
+# 1 - R$ 1.99
+# 2 - R$ 3.98
 # ...
-# 50 - R$ 9.00
+# 50 - R$ 99.50
+
+print(" Lojas Quase Dois - Tabela de preços")
+
+count = 1
+
+while count <= 50:
+    print(count, "- R$", count*1.99)
+    count += 1
+
+
+
 
 # 22. O Sr. Manoel Joaquim expandiu seus negócios para além dos negócios de 1,99 e
 # agora possui uma loja de conveniências. Faça um programa que implemente uma
